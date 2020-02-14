79592371a32337debf251ef121d806ad08c64c29
antosha-85 <antosha-85@yandex.ru>
2020-02-14 16:33:53 +0000

    Fixed multiple hidden divs rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,5 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/events"
-  }).done((events) => {
-    for(event of events) {
-      $("<div>").text(event.title).appendTo($("body"));
-    }
-  });
+  
   $( "#time" ).timepicker();
 
   function copyUrl() {
